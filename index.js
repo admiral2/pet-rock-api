a0c5b2a63015bcfe69b36404ee7118ab1627d494
Michael Humphris <michahump@gmail.com>
2017-04-09 16:15:34 +0930

    feat: added basic CORS for local development

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ var configurationRouter = require('./components/configuration/configuration.rout
 var storeRouter = require('./components/store/store.router');
 
 var server = restify.createServer();
+
+server.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 configurationRouter.applyRoutes(server, '/api/config');
 storeRouter.applyRoutes(server, '/');
 
-server.listen(8080, function() {
+server.listen(8090, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
